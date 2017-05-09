ec2f8de57b87f09f25a160cdbd31fef42230c22c
jeiso <jcastroladino@gmail.com>
2017-05-09 12:28:44 -0500

    Create recepcion.php

---
+++ recepcion.php
@@ -0,0 +1,16 @@
+<?php
+ $correo=$_GET['correo'];
+ $edad=$_GET['edad'];
+ 
+ echo "se ha registrado el se&ntilde;or ".$correo. "";
+
+if($edad>40)
+{
+echo " eres cuchito";
+}else{
+ echo "eres un culicagado";
+}
+
+
+
+?>
